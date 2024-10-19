096b951e84cdb3e6634caaafb773f8cc0e634cfc
André <48385244+estavadormir@users.noreply.github.com>
2024-10-19 23:18:41 +0200

    feat: Implement usage tracking for the used dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import inquirer from "inquirer"; // Import inquirer
 import path from "path";
 import { checkDependencies, getAllDeps } from "./dependencyChecker";
 import {
-    analyzeImportsWithSwc,
-    getAllFiles,
-    loadIgnorePatterns,
+  analyzeImportsWithSwc,
+  getAllFiles,
+  loadIgnorePatterns,
 } from "./fileAnalyser";
 
 async function getSourceDirectory() {
@@ -33,25 +33,39 @@ async function main() {
   const allFiles = getAllFiles(resolvedDir, [], ignorePatterns); // Get all files in the specified directory
 
   // Check dependencies for the specified project directory
-  const { dependencies, devDependencies } = await checkDependencies(resolvedDir); // Pass resolved directory to checkDependencies
+  const { dependencies, devDependencies } =
+    await checkDependencies(resolvedDir); // Pass resolved directory to checkDependencies
   console.log("Dependencies:", dependencies);
   console.log("DevDependencies:", devDependencies);
 
   // Gather all imports from the analyzed files
   const allImports: string[] = [];
 
+  // Create a usage counter for each dependency
+  const usageCount: Record<string, number> = {};
+
   // Analyze imports for each file
   for (const file of allFiles) {
     const imports = await analyzeImportsWithSwc(file);
-    console.log(`Imports in ${file}:`, imports);
     allImports.push(...imports); // Aggregate imports into the allImports array
+    // Update usage counter for each imported dependency
+    for (const imp of imports) {
+      if (usageCount[imp] !== undefined) {
+        usageCount[imp] += 1; // Increment count if already present
+      } else {
+        usageCount[imp] = 1; // Initialize count
+      }
+    }
   }
 
   // Identify unused dependencies
-  const unusedDeps = getAllDeps(dependencies, devDependencies).all.filter(dep => !allImports.includes(dep));
+  const unusedDeps = getAllDeps(dependencies, devDependencies).all.filter(
+    (dep) => !allImports.includes(dep),
+  );
 
   // Report unused dependencies
   if (unusedDeps.length) {
+    console.log("Usage Count:", usageCount);
     console.log("Unused Dependencies:", unusedDeps);
   } else {
     console.log("All dependencies are being used!");
